f95f96a6b75fc5275705b423991f360263530d86
owanr <risakoowan@gmail.com>
2023-06-12 16:41:45 -0500

    current progress

--- scripts/combine_annotations.py
+++ scripts/combine_annotations.py
@@ -1,5 +1,6 @@
 import json 
 import random
+import os
 random.seed(42)
 
 
@@ -15,7 +16,7 @@ def get_url_from_bbc_id(bbc_ids):
     with open("./XSum-Dataset/XSum-WebArxiveUrls.txt") as f:
         urls = f.readlines()
     filtered_urls = []
-    url_bbc_ids = [url[-9:].strip() for url in urls]
+    url_bbc_ids = [url.strip()[-8:] for url in urls]
     for i, bbc_id in enumerate(url_bbc_ids):
         if bbc_id in bbc_ids:
             filtered_urls.append(urls[i])
@@ -24,9 +25,35 @@ def get_url_from_bbc_id(bbc_ids):
     assert len(filtered_urls) == len(bbc_ids), "Number of selected urls and bbc_ids do not match"
     return filtered_urls
 
-bbc_ids = get_x_bbc_ids(20)
-filtered_urls = get_url_from_bbc_id(bbc_ids)
-print(filtered_urls)
+def get_data_from_urls():
+    '''
+    Arg: dir containing extracted text, summary, and urls
+    Returns: dict containing text, summary, and factuality annotations
+    '''
+    with open("./XSum-Dataset/XSum-WebArxiveUrls_Filtered.txt") as f:
+        urls = f.readlines()
+        urls = [url.strip() for url in urls]
+        bbc_ids = [url.strip()[-8:] for url in urls]
+    data = {}
+    # get all files from dir
+    dirname = "./XSum-Dataset/xsum-extracts-from-downloads-filtered/"
+    for file in os.listdir(dirname):
+        if file.endswith(".data"):
+            with open(os.path.join(dirname, file)) as f:
+                content = f.readlines()
+                url = content[1].strip()
+                if url in urls:
+                    summary_ind = content.index("[XSUM]INTRODUCTION[XSUM]\n") + 1
+                    text_ind = content.index("[XSUM]RESTBODY[XSUM]\n") + 1
+                    summary = ' '.join(text[summary_ind:text_ind-1])
+                    text = ' '.join(text[text_ind:])
+                    data[url] = {"summary": summary, "text": text}
+                print(text[1].strip())
+                return
+#bbc_ids = get_x_bbc_ids(20)
+#filtered_urls = get_url_from_bbc_id(bbc_ids)
+get_data_from_urls()
+#print(filtered_urls)
 
 def main():
     with open("./XSum-Dataset/chosen_urls.txt", "r") as f:
